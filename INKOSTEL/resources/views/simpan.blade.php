26dccb5a57f13421e00b118c0f55bcac80cd9174
RIPO12345 <placida.ripo.naibaho@gmail.com>
2023-12-04 10:05:45 +0700

    Ripo

--- INKOSTEL/resources/views/simpan.blade.php
+++ INKOSTEL/resources/views/simpan.blade.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>InKosTel Cari Kost</title>
+    <title>InKosTel simpan kost</title>
 
     <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}"> 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="{{ asset('css/carikost.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/simpan.css') }}">
   </head>
   <body>
 
@@ -117,14 +117,6 @@
     </header>
     <!--Navbar End-->
 
-    <!-- Tombol Filter Pencarian -->
-    <div class="filter-button">
-        <button type="button" class="btn" data-filter="semua">Semua</button>
-        <button type="button" class="btn" data-filter="terdekat">Terdekat</button>
-        <button type="button" class="btn" data-filter="termurah">Termurah</button>
-        <button type="button" class="btn" data-filter="putra">Putra</button>
-        <button type="button" class="btn" data-filter="putri">Putri</button>
-    </div>
 
 
     <!-- main -->
@@ -247,9 +239,8 @@
 
     <!-- script js -->
 
-    <script src="{{ asset('js/carikost.js') }}"></script>
+    <script src="{{ asset('js/simpan.js') }}"></script>
   </body>
 </html>
 
- 
 
