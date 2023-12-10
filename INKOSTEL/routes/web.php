801843768ab83907db60b8c77662fb26185682cf
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2023-12-11 00:41:23 +0800

    update web.php

--- INKOSTEL/routes/web.php
+++ INKOSTEL/routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('JualKos');
+    return view('index');
 });
 
 Route::get('/login', [LoginController::class, 'index']);
