9dcd70f7c3e2621ca6e53e817c187d34bd1c2b1c
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2023-12-17 15:47:45 +0800

    Update 2023_12_11_110610_create_carikos_table.php

--- INKOSTEL/database/migrations/2023_12_11_110610_create_carikos_table.php
+++ INKOSTEL/database/migrations/2023_12_11_110610_create_carikos_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->integer('jarak_kos');
             $table->mediumText('gambar_kos');
             $table->string('alamat');
-            $table->string('jarak');
             $table->string('Deskripsi');
             $table->string('Fasilitas');
             $table->string('ContactPerson');
