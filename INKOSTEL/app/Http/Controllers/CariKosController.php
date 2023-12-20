aa55eb801d83a80680fd4ab7d281ae8f26b03ecc
rifqi10 <den.rifqihafid@gmail.com>
2023-12-20 17:22:41 +0700

    apdet

--- INKOSTEL/app/Http/Controllers/CariKosController.php
+++ INKOSTEL/app/Http/Controllers/CariKosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\CariKos;
+use App\Helpers\Helpers;
 
 class CariKosController extends Controller
 {
@@ -12,6 +13,7 @@ class CariKosController extends Controller
         $carikos = CariKos::all();
         return view('carikost', compact(['carikos']));
     }
+    
 
     public function simpan(){
         //
