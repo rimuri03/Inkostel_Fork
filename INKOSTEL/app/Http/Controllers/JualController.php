8a09756bd304ed27b549ce1efe9beb564009c2ce
mghazyfh <127060793+mghazyfh@users.noreply.github.com>
2023-12-20 16:57:33 +0700

    update jualkos

--- INKOSTEL/app/Http/Controllers/JualController.php
+++ INKOSTEL/app/Http/Controllers/JualController.php
@@ -20,37 +20,31 @@ class JualController extends Controller
         $request->validate([
             "nama_kos" => 'required|string|max:255',
             "alamat" => 'required|string|max:255',
-<<<<<<< Updated upstream
-            "jarak_kos" => 'required|string',
-            "ContactPerson" => 'required|string',
-            "Fasilitas" => 'required|array',
-            "harga_kos_perbulan" => 'required|string',
-            "harga_kos_pertahun" => 'required|string',
-=======
+
             "jarak_kos" => 'required|in:0-100_meter,100-500_meter,500-1000_meter',
             "ContactPerson" => 'required|string',
             // "Fasilitas" => 'required|array',
             "harga_kos_pertahun" => 'required|string',
             'harga_kos_perbulan'=> 'string',
->>>>>>> Stashed changes
+
             "gambar_kos" => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
             "Deskripsi" => 'required|string',
         ]);
 
         // Store the seller information to the database
         $jual = new jual();
-<<<<<<< Updated upstream
+
         $jual->name = $request->input('nama_kos'); 
         $jual->alamat = $request->input('alamat');
         $jual->jarak = $request->input('jarak_kos');
         $jual->nomor_telepon = $request->input('ContactPerson');
-=======
+
         $jual->nama_kos = $request->input('nama_kos'); 
         $jual->alamat = $request->input('alamat');
         $jual->jarak_kos = $request->input('jarak_kos');
         $jual->ContactPerson = $request->input('ContactPerson');
         // $jual->fasilitas = implode(',', $request->input('Fasilitas'));
->>>>>>> Stashed changes
+
         $jual->harga_kos_pertahun = $request->input('harga_kos_pertahun');
         $jual->harga_kos_perbulan = $request->input('harga_kos_perbulan');
 
@@ -64,9 +58,9 @@ class JualController extends Controller
         // Redirect to a success page or back to the form with a success message
         return response()->json(['message' => 'Item berhasil disimpan']);
     }
-<<<<<<< Updated upstream
-=======
+
+
 
     
->>>>>>> Stashed changes
+
 }
