99a5eb9830c393ce292d8f833f7c5a6fa39d1505
MNFI13-blode <naufaltaurus24@gmail.com>
2023-12-20 15:40:18 +0700

    profile

--- INKOSTEL/resources/views/profile.blade.php
+++ INKOSTEL/resources/views/profile.blade.php
@@ -21,15 +21,13 @@
             <div class="row no-gutters row-bordered row-border-light">
                 <div class="col-md-12 pt-10">
                     <form action="/update" method="post">
-                        @csrf
-                        @foreach($profile as $p)
                         <div class="container-fluid">
-                        <h1 class="font-weight-bold" style="padding-bottom: 50px;padding-top: 20px;">Hai, Supri Kowalski</h1>
-                        <h4 class="font-weight-bold">Profile Picture</h4>
+                            <h1 class="font-weight-bold" style="padding-bottom: 50px;padding-top: 20px;">Hai, Supri Kowalski</h1>
+                            <h4 class="font-weight-bold">Profile Picture</h4>
                         </div>
                         <div class="card-body d-flex align-items-center">
                             <div>
-                                <img src="{{ $p->foto_profil }}" alt="Profile Picture" class="profile-pict rounded float-left">
+                                <img src="{{ asset('img/WhatsApp Image 2023-10-29 at 12.35 1.png') }}" alt="Profile Picture" class="profile-pict rounded float-left">
                             </div>
                             <div class="media-body ms-4" style="padding-bottom: 40px;">
                                 <div class="input-group">
@@ -38,7 +36,7 @@
                                             <label class="btn btn-outline-primary">
                                                 <span class="bi bi-pencil-square"></span>
                                                 <span class="text-color">Edit</span>
-                                                <input type="file" style="display: none;" id="foto_profil">
+                                                <input type="file" style="display: none;">
                                             </label>
                                         </form>                                    
                                         <p class="p">Foto harus berformat .png/.jpg</p>
@@ -52,19 +50,19 @@
                         <div class="card-body">
                             <label class="form-label"><h6 class="font-weight-bold" style="padding-top: 10px;">Nama Lengkap</h6></label>
                             <div class="form-group">
-                                <input type="text" class="form-control mb-1" value="{{ $p->nama_lengkap }}" id="nama_lengkap" oninput="changeButtonColor()">
+                                <input type="text" class="form-control mb-1" value="Supri Makmur" id="nameInput" oninput="changeButtonColor()">
                             </div>
                             <div id="nameWarning" class="text-danger" style="padding-top: 10px;"></div>
                         
                             <label class="form-label"><h6 class="font-weight-bold" style="padding-top: 15px;">Username</h6></label>
                             <div class="form-group">
-                                <input type="text" class="form-control mb-1" value="{{ $p->username }}" id="username" oninput="changeButtonColor()" readonly>
+                                <input type="text" class="form-control mb-1" value="supri.maklo" id="usernameInput" oninput="changeButtonColor()">
                             </div>
                             <div id="usernameWarning" class="text-danger" style="padding-top: 10px;"></div>
                         
                             <label class="form-label"><h6 class="font-weight-bold"  style="padding-top: 15px;">Alamat Email</h6></label>
                             <div class="form-group">
-                                <input type="text" id="email" class="form-control" value="{{ $p->email }}" oninput="changeButtonColor()" readonly>
+                                <input type="text" id="emailInput" class="form-control" value="supri.maklo@gmail.com" oninput="changeButtonColor()">
                             </div>
                             <div id="emailWarning" class="text-danger" style="padding-top: 10px;"></div>
                         
@@ -73,16 +71,15 @@
                                 <div class="input-group-prepend">
                                     <span class="input-group-text" id="inputGroup-sizing-default">+62</span>
                                 </div>
-                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{ $p->nomor_telpon }}" id="nomor_telpon" oninput="runtwofunction()">
+                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="895 329 917 984" id="phone" oninput="runtwofunction()">
                             </div>
                             <div id="phoneWarning" class="text-danger" style="padding-top: 10px;"></div>
                             
                             <div style="padding-bottom: 50px; padding-top: 25px;">
                                 <button type="button" class="btn btn-outline-primary2" id="saveButton" onclick="saveForm()">Simpan</button>
                             </div>
-                        </div>
-                        @endforeach
-                    </form>                    
+                        </div>                  
+                    </form>  
                 </div>
             </div>
         </div>
