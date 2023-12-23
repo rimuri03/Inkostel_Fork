af54ddd7697e950c5036b89176f0a5e74a7e7796
RIPO12345 <placida.ripo.naibaho@gmail.com>
2023-12-23 22:37:50 +0700

    Ripo Simpan

--- INKOSTEL/app/Http/Controllers/SimpanController.php
+++ INKOSTEL/app/Http/Controllers/SimpanController.php
@@ -16,21 +16,4 @@ class SimpanController extends Controller
         return view('simpan', compact('dataKos'));
     }
 
-    public function delete($id)
-    {
-        // Temukan data Simpan berdasarkan ID
-        $simpan = Simpan::find($id);
-
-        // Jika data tidak ditemukan
-        if (!$simpan) {
-            return redirect()->route('simpan.halaman')->with('error', 'Data tidak ditemukan');
-        }
-
-        // Lakukan logika penghapusan atau hapus data
-        $simpan->delete();
-
-        // Setelah penghapusan, Anda bisa mengarahkan pengguna ke halaman lain
-        return redirect()->route('simpan.halaman')->with('success', 'Data berhasil dihapus');
-    }
-
 }
