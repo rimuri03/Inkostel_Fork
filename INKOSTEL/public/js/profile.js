73c42b6c315a85a179b2c45a3a5dbc42c3b4fb80
ahmdmuflih <ahmdmuflih@student.telkomuniversity.ac.id>
2023-12-25 01:42:07 +0700

    sidebar active ketika berada di salah satu page

--- INKOSTEL/public/js/profile.js
+++ INKOSTEL/public/js/profile.js
@@ -35,3 +35,29 @@ originalValue = currentNumber;
 document.getElementById("saveButton").style.backgroundColor = "#595959";
 }
 // Fungsi untuk membatalkan perubahan
+
+// navBar
+const navBar = document.querySelector("nav"),
+  menuBtn = document.querySelectorAll(".menu-icon"),
+  overlay = document.querySelector(".overlay");
+console.log(navBar, menuBtn, overlay);
+
+// Tambahkan class "active" pada link yang sesuai dengan halaman yang sedang aktif
+const currentPage = window.location.pathname;
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(link => {
+  if (link.getAttribute("href") === currentPage) {
+    link.classList.add("active");
+  }
+});
+
+menuBtn.forEach(menuBtn => {
+  menuBtn.addEventListener("click", () => {
+    navBar.classList.toggle("open");
+  });
+});
+
+overlay.addEventListener("click", () => {
+  navBar.classList.remove("open");
+});
