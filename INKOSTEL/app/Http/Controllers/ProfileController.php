606df16e651e0f692b7e5c342bb3db76a958cd22
MNFI13-blode <naufaltaurus24@gmail.com>
2023-12-27 12:39:33 +0700

    Update ProfileController.php

--- INKOSTEL/app/Http/Controllers/ProfileController.php
+++ INKOSTEL/app/Http/Controllers/ProfileController.php
@@ -15,26 +15,6 @@ class ProfileController extends Controller
     }
     public function update(Request $request)
     {
-        // Validasi data input hanya untuk username dan email
-        $request->validate([
-            'username' => 'required|string',
-            'email' => 'required|email',
-        ]);
-
-        // Ambil data dari formulir
-        $username = $request->input('username');
-        $email = $request->input('email');
-
-        // Update data profil di database
-        Profile::update([
-            'username' => $username,
-            'email' => $email,
-            'nama_panjang' => $nama_panjang,
-            'nomor_telpon' => $nomor_telpon,
-            // Tambahkan kolom-kolom lain yang perlu diupdate
-        ]);
-
-        // Redirect atau kirim respons sesuai kebutuhan Anda
-        return redirect()->route('profile')->with('success', 'Profil berhasil diperbarui');
+        
     }
 }
