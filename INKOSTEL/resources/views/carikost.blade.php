81398532db5365131e8dcac51026cc0c246c26c9
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2023-12-27 18:12:07 +0800

    Update carikost.blade.php

--- INKOSTEL/resources/views/carikost.blade.php
+++ INKOSTEL/resources/views/carikost.blade.php
@@ -44,6 +44,7 @@
                     </div>
 
                     <div class="card-body">
+                        <p style="display: none;">{{ $data->id_kos }}</p>
                         <h5 class="card-title">{{ $data->nama_kos}}</h5>
                         <p class="card-text1" data-harga="{{ $data->harga_kos_pertahun }}">{{ $data->harga_kos_pertahun }}</p>
                         <p id="jarak-{{ $data->id }}" class="card-text2">{{ $data->jarak_kos}}</p>
