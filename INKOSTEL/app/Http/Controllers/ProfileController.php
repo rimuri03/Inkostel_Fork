ddb62af92bcc70229f30bc9f4024ae56f9b688c4
MNFI13-blode <naufaltaurus24@gmail.com>
2024-01-09 21:32:35 +0700

    Update ProfileController.php

--- INKOSTEL/app/Http/Controllers/ProfileController.php
+++ INKOSTEL/app/Http/Controllers/ProfileController.php
@@ -24,13 +24,15 @@ class ProfileController extends Controller
     public function update(Request $request, $id)
     {
         $profileData = Profile::where('id', $id)->first();
-        $img = $request->file("file");
-        $img -> move("img/profile",$img->getClientOriginalName());
+        if ($request->hasFile("file")) {
+            $img = $request->file("file");
+            $img->move("img/profile", $img->getClientOriginalName());
+            $profileData->foto_profil = $img->getClientOriginalName();
+        }
         $profileData->username = $request->username;
         $profileData->email = $request->email;
         $profileData->nama_panjang = $request->nama_panjang;
         $profileData->nomor_telpon = $request->nomor_telpon;
-        $profileData->foto_profil = $img->getClientOriginalName();
         $profileData->save();
         return redirect('profile')->with('success', 'Data Berhasil Diperbarui');
     }
